c250837574b9cf956b593668ecbfdca8d6dd73c0
Andy <349562769@gapps.yrdsb.ca>
2022-08-27 19:43:17 -0400

    file form

--- client/src/Main.js
+++ client/src/Main.js
@@ -10,7 +10,7 @@ function Main (){
     const Container = styled.div`
         background-color: ${colors.lightBeige};
         height: 1080px;
-
+        border-top:1px ${colors.lightBeige} solid;
     `;
 
     const Dropbox = styled.div`
@@ -28,6 +28,7 @@ function Main (){
         text-align: center;
         position: relative;
         bottom: 0px;
+        border:1px ${colors.mocha} solid;
     `
     const Logo = styled.div `
         float:left;
@@ -38,6 +39,23 @@ function Main (){
         background-repeat: no-repeat;
         background-size: 100% 100%;
     `;
+    const UploadFile = styled.div `
+        display:flex;
+        flex-direction: column;
+        width = 100%;
+        justify-content:center;
+    `;
+    const UploadButton = styled.button`
+    
+    `;
+    const [files, setFiles] = useState([]);
+    const onInputChange = (e) => {
+        setFiles(e.target.files)
+    };
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        console.log(files);
+    }
     return (
         <body>
             <Container>
@@ -45,11 +63,18 @@ function Main (){
                     <h1 font-size= "10px">
                         dropbox
                     </h1>
+                    <form method="post" onSubmit={onSubmit}>
+                        <UploadFile>
+                            <label>Upload File:</label>
+                            <input type="file" onChange = {onInputChange} />
+                        </UploadFile>
+                        <UploadButton>Submit</UploadButton>
+                    </form>
                 </Dropbox>
                 
             </Container>
             <Footer>
-                <Logo img = {gitLogo}>
+                <Logo img = {gitLogo} >
 
                 </Logo>
             </Footer>
