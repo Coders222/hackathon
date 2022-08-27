c69928f9c48a093adb631777cd286822b3aa8a3c
Andy <349562769@gapps.yrdsb.ca>
2022-08-27 14:12:20 -0400

    added Images

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -6,12 +6,20 @@ const {colors} = theme;
 function Nav (){
     const Container = styled.nav `
         min-width : 100vw;
-        height:50px;
+        height:75px;
         background-color: ${colors.lightBeige};
+    `;
+    const GitLogo = styled.div `
+        width: 50px;
+        height = 50px;
     `;
     return (
         <Container>
-            
+            <GitLogo>
+                <img src='./images/gitLogo.png'>
+
+                </img>
+            </GitLogo>
         </Container>
     );
 }
