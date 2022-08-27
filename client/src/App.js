c758430f6675b37194c2bbd8317a2b6e873d90b9
Orion <orion.chen@hotmail.com>
2022-08-27 11:29:05 -0400

    added orion was here

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@
 import './App.css';
 
 function App() {
+  console.log("orion was here");
   return (
     <div className="App">
       <h1>
         Test
       </h1>
     </div>
+
+   
   );
+  
 }
 
 export default App;
