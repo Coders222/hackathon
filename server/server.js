ffb4a7a0bdbb3e317a3e5af88f5bd1f0748815ab
Andy <349562769@gapps.yrdsb.ca>
2022-08-27 21:46:10 -0400

    backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,35 @@
 var express = require('express');
+const multer = require('multer');
+const cors = require('cors');
 const app = express();
+app.use(cors());
+app.use(express.static('public'));
 const port = 8888;
 app.get('/', (req, res) => {
     res.send('hello people');
+});
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+});
+
+const upload = multer({storage}).array('file');
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            return res.status(500).json(err)
+        }
+
+        return res.status(200).send(req.files)
+    })
+    console.log(req);
 });
 app.listen(port, () => {
     console.log('listening to the port: ' + port);
